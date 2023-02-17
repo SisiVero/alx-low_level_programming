c32c1dca96cb9d395218a204dfa9c7998ba61e0d
SisiVero <csusan894@gmail.com>
2023-02-17 08:26:06 -0800

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+/**
+ * main - entrt poiny
+ *Return: Always (Success)
+ */
+
+ int main(void)
+ {
+        char low, e, q;
+
+        e = 'e';
+        q = 'q';
+
+        for (low = 'a'; low <= 'z';low++)
+	{
+	if (low != e && low != q)
+	putchar(low);
+	}
+	putchar('\n');
+	return (0);
+ }
+
+	
