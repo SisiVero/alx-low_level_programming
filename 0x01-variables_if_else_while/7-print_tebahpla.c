b1cf5a2db906978c2dc0c4e55ba609ba8647d6a8
SisiVero <csusan894@gmail.com>
2023-02-17 09:05:33 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+	putchar(low);
+	putchar('\n');
+	return (0);
+}
