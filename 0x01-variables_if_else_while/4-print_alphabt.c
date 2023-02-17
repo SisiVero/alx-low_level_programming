9548336344ba30e027d335a47244f9ad15515f08
SisiVero <csusan894@gmail.com>
2023-02-17 08:31:31 -0800

    Firs correction to Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,20 +9,18 @@
  *Return: Always (Success)
  */
 
- int main(void)
- {
-        char low, e, q;
+int main(void)
+{
+	char low, e, q;
 
-        e = 'e';
-        q = 'q';
-
-        for (low = 'a'; low <= 'z';low++)
+	e = 'e';
+	q = 'q';
+	
+	for (low = 'a'; low <= 'z';low++)
 	{
 	if (low != e && low != q)
 	putchar(low);
 	}
 	putchar('\n');
 	return (0);
- }
-
-	
+}	
